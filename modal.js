365a52d8c79f294ce8d2ca895f18512e71864fc9
MigVarona <migvaronag@gmail.com>
2024-11-13 21:50:14 +0100

    pr

--- modal.js
+++ modal.js
@@ -1,102 +1,22 @@
-function openModal() {
-    document.getElementById("myModal").style.setProperty('display', 'flex', 'important');
+function openModal(modalId) {
+    document.getElementById(modalId).style.setProperty('display', 'flex', 'important');
 }
 
-function closeModal() {
-    var iframe = document.getElementById("iframe");
-    iframe.src = iframe.src; 
-    document.getElementById("myModal").style.display = "none";
-}
-
-window.onclick = function(event) {
-    var modal = document.getElementById("myModal");
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-function openModal2() {
-    document.getElementById("myModal2").style.setProperty('display', 'flex', 'important');
-}
-
-function closeModal2() {
-    var iframe = document.getElementById("iframe2");
-    iframe.src = iframe.src; 
-    document.getElementById("myModal2").style.display = "none";
+function closeModal(modalId, iframeId) {
+    var iframe = document.getElementById(iframeId);
+    iframe.src = iframe.src; // Reinicia el src del iframe para detener el video
+    document.getElementById(modalId).style.display = "none";
 }
 
 window.onclick = function(event) {
-    var modal = document.getElementById("myModal2");
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-function openModal3() {
-    document.getElementById("myModal3").style.setProperty('display', 'flex', 'important');
-}
-
-function closeModal3() {
-    var iframe = document.getElementById("iframe3");
-    iframe.src = iframe.src; 
-    document.getElementById("myModal3").style.display = "none";
-}
-
-window.onclick = function(event) {
-    var modal = document.getElementById("myModal3");
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-function openModal4() {
-    document.getElementById("myModal4").style.setProperty('display', 'flex', 'important');
-}
-
-function closeModal4() {
-    var iframe = document.getElementById("iframe4");
-    iframe.src = iframe.src; 
-    document.getElementById("myModal4").style.display = "none";
-}
-
-window.onclick = function(event) {
-    var modal = document.getElementById("myModal4");
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-function openModal5() {
-    document.getElementById("myModal5").style.setProperty('display', 'flex', 'important');
-}
-
-function closeModal5() {
-    var iframe = document.getElementById("iframe5");
-    iframe.src = iframe.src; 
-    document.getElementById("myModal5").style.display = "none";
-}
-
-window.onclick = function(event) {
-    var modal = document.getElementById("myModal5");
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-
-function openModal6() {
-    document.getElementById("myModal6").style.setProperty('display', 'flex', 'important');
-}
-
-function closeModal6() {
-    var iframe = document.getElementById("iframe6");
-    iframe.src = iframe.src; 
-    document.getElementById("myModal6").style.display = "none";
-}
-
-window.onclick = function(event) {
-    var modal = document.getElementById("myModal6");
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+    var modals = document.querySelectorAll('.modal'); // Asegúrate de que todos tus modales tengan la clase "modal"
+    modals.forEach(function(modal) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+            var iframe = modal.querySelector("iframe");
+            if (iframe) {
+                iframe.src = iframe.src; 
+            }
+        }
+    });
+};
